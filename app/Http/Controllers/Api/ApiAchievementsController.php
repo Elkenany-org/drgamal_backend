bea0b59178c7caced25d236f9e22e51a380ae6ee
marwan7878 <marawan20191700620@cis.asu.edu.eg>
2023-07-26 12:12:57 +0300

    api of achivements page

--- app/Http/Controllers/Api/ApiAchievementsController.php
+++ app/Http/Controllers/Api/ApiAchievementsController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Achievement;
+use App\Models\Image;
 class ApiAchievementsController extends Controller
 {
     //
     public function index()
     {
         $achievements = Achievement::latest()->get();
-        $mainImage = Image::where('type','الاخبار')->get();
-        return response()->json(['data'=>$achievements,'mainImage'=>$mainImage], 200);
+        $mainImage = Image::where('type','الاخبار')->first();
+        return response()->json(['data'=>$achievements,'mainImage'=>$mainImage->image_url], 200);
     }
 }
